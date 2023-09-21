30910e4d60cab9f57b7f778e896d2897479ec7d4
Carlos Garcia <111895268+Carlos-CGS@users.noreply.github.com>
2023-09-21 12:40:23 -0300

    Add files via upload

---
+++ Conceitos Basicos NoSql - Bootcamp DIO/ConceitosNoSQL.js
@@ -0,0 +1,233 @@
+use viagens;
+
+db.createCollection("usuarios")
+db.createCollection("destinos")
+
+// Ou vc pode inserir diretamente um documento e ele já ira criar a collection
+db.usuarios_novo.insertOne({});
+
+// Inserindo o primeiro documento
+db.usuarios.insertOne(
+    {   
+        "nome": "nome",
+        "data_nascimento": "1990-10-05",
+        "email": "pamela.apolinario.borges@gmail.com",
+        "endereco": "Av Manoel Marques de Jesus, 380 - Vila Xavier, Araraquara/SP"
+    });
+
+db.usuarios.insertMany([
+    {   
+        "nome": "Pamela",
+        "idade": 30,
+        "email": "pamela.apolinario.borges@gmail.com",
+        "endereco": "Av Manoel Marques de Jesus, 380 - Vila Xavier, Araraquara/SP"
+    },
+    {   
+        "nome": "Pamela",
+        "idade": 31,
+        "email": "pamela.apolinario.borges.outra@gmail.com",
+        "endereco": "Av Manoel Marques de Jesus, 380 - Vila Xavier, Araraquara/SP"
+    },
+]);
+
+
+db.destinos.insertOne({"nome":"Praia do Rosa", "descricao":"LInda praia"})
+
+//Inserindo mais usuarios
+
+// Inserir documentos na coleção "usuarios"
+db.usuarios.insertMany([{
+    nome: "João",
+    idade: 25,
+    cidade: "São Paulo",
+    estado: "SP",
+    endereco: {
+      rua: "Avenida Principal",
+      numero: 123,
+      cidade: "São Paulo",
+      estado: "SP"
+    }
+  }, {
+    nome: "Maria",
+    idade: 30,
+    cidade: "Rio de Janeiro",
+    estado: "RJ",
+    endereco: {
+      rua: "Rua Secundária",
+      numero: 456,
+      cidade: "Rio de Janeiro",
+      estado: "RJ"
+    }
+},{
+    nome: "Carlos",
+    idade: 20,
+    cidade: "São Paulo",
+    estado: "SP",
+    endereco: {
+      rua: "Rua Principal",
+      numero: 789,
+      cidade: "São Paulo",
+      estado: "SP"
+    }
+  },{
+    nome: "Ana",
+    idade: 35,
+    cidade: "São Paulo",
+    estado: "SP",
+    endereco: {
+      rua: "Avenida Secundária",
+      numero: 1011,
+      cidade: "São Paulo",
+      estado: "SP"
+    }
+    }
+    ,  
+    {
+    nome: "Pedro",
+    idade: 28,
+    cidade: "Belo Horizonte",
+    estado: "MG",
+    endereco: {
+      rua: "Rua Principal",
+      numero: 1314,
+      cidade: "Belo Horizonte",
+      estado: "MG"
+    }
+  }]);
+  
+
+// Find
+db.usuarios.find({});
+db.usuarios.find({"nome": "João"});
+db.usuarios.findOne({"nome": "João"});
+db.usuarios.findOneAndUpdate({ nome: "João" }, { $set: { idade: 26 } });
+db.usuarios.findOneAndDelete({ nome: "João" });
+
+// Update
+db.usuarios.updateOne(
+  { nome: "João" },
+  { $set: { idade: 26 } }
+);
+db.usuarios.updateMany(
+  { cidade: "São Paulo" },
+  { $set: { estado: "SP" } }
+);
+db.usuarios.replaceOne(
+  { nome: "João" },
+  {
+    nome: "John",
+    idade: 27,
+    cidade: "São Paulo",
+    estado: "SP",
+    endereco: {
+      rua: "Avenida Principal",
+      numero: 123
+    }
+  }
+);
+
+// Update Operadores
+// Usando o operador $set para definir o valor de um campo específico
+db.usuarios.updateOne({ nome: "João" }, { $set: { idade: 26 } });
+
+// Usando o operador $inc para incrementar o valor de um campo numérico
+db.usuarios.updateOne({ nome: "João" }, { $inc: { idade: 1 } });
+
+// Usando o operador $rename para renomear um campo existente
+db.usuarios.updateOne({ nome: "João" }, { $rename: { "endereco.rua": "endereco.nomeRua" } });
+
+// Usando o operador $unset para remover um campo específico de um documento
+db.usuarios.updateOne({ nome: "João" }, { $unset: { endereco: "" } });
+
+// Delete
+// Usando o método deleteOne() para excluir o primeiro documento que corresponde ao filtro especificado
+db.usuarios.deleteOne({ nome: "João" });
+
+// Usando o método deleteMany() para excluir todos os documentos que correspondem ao filtro especificado
+db.usuarios.deleteMany({ cidade: "São Paulo" });
+
+// Operadores Lógicos
+db.usuarios.find({ $and: [{ idade: { $gte: 18 } }, { cidade: "São Paulo" }] });
+
+db.usuarios.find({ $or: [{ idade: { $lt: 18 } }, { cidade: "Rio de Janeiro" }] });
+
+db.usuarios.find({ idade: { $not: { $eq: 25 } } });
+
+// Operadores de Comparação
+db.usuarios.find({ idade: { $eq: 25 } });
+
+db.usuarios.find({ idade: { $ne: 30 } });
+
+db.usuarios.find({ idade: { $gt: 30 } });
+
+db.usuarios.find({ idade: { $gte: 30 } });
+
+db.usuarios.find({ idade: { $lt: 30 } });
+
+db.usuarios.find({ idade: { $lte: 30 } });
+
+db.usuarios.find({ cidade: { $in: ["São Paulo", "Rio de Janeiro"] } });
+
+db.usuarios.find({ cidade: { $nin: ["São Paulo", "Rio de Janeiro"] } });
+
+// Projeção
+db.usuarios.find({}, { nome: 1, idade: 1 })
+
+// Ordenação
+db.usuarios.find().sort({ idade: 1 })
+
+// Limitação
+db.usuarios.find().limit(10)
+
+// Paginação
+db.usuarios.find().skip(10).limit(5)
+
+
+// ***************** Iserção de Dados - Formato JSON *******************
+{   
+    "_id": "",
+    "nome": "nome",
+    "data_nascimento": "1990-10-05",
+    "email": "pamela.apolinario.borges@gmail.com",
+    "endereco": "Av Manoel Marques de Jesus, 380 - Vila Xavier, Araraquara/SP"
+}
+// Ou
+{   
+    "_id": "",
+    "nome": "nome",
+    "data_nascimento": "1990-10-05",
+    "email": "pamela.apolinario.borges@gmail.com",
+    "endereco": {
+        "rua": "Av Manoel",
+        "numero": 123,
+        "cidade": "Araraquara",
+        "estado": "SP"
+    }
+}
+// ou
+{   
+    "_id": "",
+    "nome": "nome",
+    "data_nascimento": "1990-10-05",
+    "email": "pamela.apolinario.borges@gmail.com",
+    "enderecos": [
+        {
+        "rua": "Av Manoel",
+        "numero": 123,
+        "cidade": "Araraquara",
+        "estado": "SP"
+        },
+        {
+        "rua": "Av Do estado",
+        "numero": 123,
+        "cidade": "Araraquara",
+        "estado": "SP"
+        },
+    ]
+}
+
+{
+    "_id": "",
+    "nome": "nome destino",
+    "descricao": "Descricao do destino"
+}
---
+++ Conceitos Basicos NoSql - Bootcamp DIO/README.md
@@ -0,0 +1,5 @@
+# Conceitos Básicos sobre o uso do NoSQL
+
+- Explicação e exemplificação dos conceitos básicos sobre o uso do NoSQL utilizando o MongoDB, inserindo arquivo em formato JSON.
+
+- Inserindo documentos, inserindo um lista de documentos a coleção, comandos FIND, UPDATE, DELETE, projeção, ordenação, limitação e paginação. 
